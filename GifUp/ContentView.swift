81847f42302044e4e22dd6e8ebd532a29a55e334
Nathan Lawrence <nathan.lawrence.barrick@gmail.com>
2020-03-19 14:51:07 -0500

    Initial Commit

---
+++ GifUp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GifUp
+//
+//  Created by Nathan Lawrence on 3/19/20.
+//  Copyright © 2020 Team Nighthawk, LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
